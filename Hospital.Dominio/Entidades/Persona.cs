0e85b4f43d445c5a25f156b9462aab7ade3daa3c
Cesar Augusto Rodriguez Diaz <cesaresmas.au@gmail.com>
2021-10-24 17:12:11 -0500

    se creo la referencia de persistencia y se creo la carpeta entidades con la clase persona

---
+++ Hospital.Dominio/Entidades/Persona.cs
@@ -0,0 +1,8 @@
+namespace Hospital.Dominio.Entidades
+{
+    public class Persona//aca ponemos las entidades se crearan las tablas con los datos de la base de datos
+    {
+        public int Id{get; set;}// el id lo reconoce como primary key
+        public string Nombre{get; set;}
+    }
+}
